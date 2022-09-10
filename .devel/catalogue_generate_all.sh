6e4e302898a9274d89d859cbf0bed410f3632ab6
gagolews <m.gagolewski@gmail.com>
2022-09-10 20:21:41 +1000

    v1.0.1

---
+++ .devel/catalogue_generate_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyleft (C) 2018-2022, Marek Gagolewski <https://www.gagolewski.com>
+
+# please run from within the repo's root folder
+
+batteries="fcps graves other sipu uci wut mnist h2mg g2mg"
+for b in $batteries; do
+    ../catalogue_generate.py $b
+    #pandoc .catalogue/$b.md --to html -o .catalogue/$b.html
+done
